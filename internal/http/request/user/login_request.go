9c720ada768c358ea6b630312f87128c00727db6
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-11-19 06:13:16 +0000

    cleaning up the code

--- internal/http/request/user/login_request.go
+++ internal/http/request/user/login_request.go
@@ -4,10 +4,3 @@ type LoginRequest struct {
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
-
-// package request
-
-// type LoginWebRequest struct {
-// 	Email    string `form:"email" binding:"required"`
-// 	Password string `form:"password" binding:"required"`
-// }
