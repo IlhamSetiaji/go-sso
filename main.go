6afa2508cb9c797a00d26b55dda2c04c9dd5d59e
faisolavolut <faisol@avolut.com>
2024-12-04 16:06:43 +0700

    up

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 	"net/http"
 	"strconv"
 
-	"github.com/IlhamSetiaji/go-rabbitmq-utils/rabbitmq"
 	"github.com/gin-contrib/sessions"
 	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-gonic/gin"
@@ -42,11 +41,11 @@ func main() {
 		log.Printf("Failed to initialize the zitadel authenticator: %v", err)
 	}
 
-	err = rabbitmq.InitializeConnection(viperConfig.GetString("rabbitmq.url"))
-	if err != nil {
-		log.Fatalf("Failed to connect to RabbitMQ: %v", err)
-	}
-	defer rabbitmq.CloseConnection()
+	// err = rabbitmq.InitializeConnection(viperConfig.GetString("rabbitmq.url"))
+	// if err != nil {
+	// 	log.Fatalf("Failed to connect to RabbitMQ: %v", err)
+	// }
+	// defer rabbitmq.CloseConnection()
 
 	// setup gin engine
 	app := gin.Default()
