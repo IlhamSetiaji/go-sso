e2d239b0c741b49352e8e6619ff92cb00bbd3896
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2024-12-27 14:53:29 +0700

    Update mobile phone validation to be optional in user creation and update requests

--- internal/http/request/web/user/create_user_request.go
+++ internal/http/request/web/user/create_user_request.go
@@ -9,7 +9,7 @@ type CreateUserRequest struct {
 	Email       string            `form:"email" validate:"required,email"`
 	Username    string            `form:"username" validate:"required"`
 	Gender      entity.UserGender `form:"gender" validate:"required,userGender"`
-	MobilePhone string            `form:"mobile_phone" validate:"required,numeric,min=10,max=13,startswith=62"`
+	MobilePhone string            `form:"mobile_phone" validate:"omitempty,numeric,min=10,max=13,startswith=62"`
 	RoleID      string            `form:"role_id" validate:"required,uuid"`
 	Status      entity.UserStatus `form:"status" validate:"required,userStatus"`
 }
--- internal/http/request/web/user/update_user_request.go
+++ internal/http/request/web/user/update_user_request.go
@@ -8,7 +8,7 @@ type UpdateUserRequest struct {
 	Email       string            `form:"email" validate:"required,email"`
 	Username    string            `form:"username" validate:"required"`
 	Gender      entity.UserGender `form:"gender" validate:"required,userGender"`
-	MobilePhone string            `form:"mobile_phone" validate:"required,numeric,min=10,max=13,startswith=62"`
+	MobilePhone string            `form:"mobile_phone" validate:"omitempty,numeric,min=10,max=13,startswith=62"`
 	Status      entity.UserStatus `form:"status" validate:"required,userStatus"`
 	RoleID      string            `form:"role_id" validate:"omitempty,uuid"`
 }
