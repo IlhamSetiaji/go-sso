09f1cf279f5aa152f9a039c5789ba5ae1447e7eb
IlhamSetiaji <ilham.ahmadz18@gmail.com>
2025-01-03 08:22:13 +0700

    feat: Add Chinese job name translation to employee response

--- internal/http/dto/employee_dto.go
+++ internal/http/dto/employee_dto.go
@@ -3,12 +3,17 @@ package dto
 import (
 	"app/go-sso/internal/entity"
 	"app/go-sso/internal/http/response"
+	"fmt"
 
 	gt "github.com/bas24/googletranslatefree"
 )
 
 func ConvertToSingleEmployeeResponse(employee *entity.Employee) *response.EmployeeResponse {
-	result, _ := gt.Translate(employee.EmployeeJob.Job.Name, "en", "zh")
+	chinese, err := gt.Translate(employee.EmployeeJob.Job.Name, "en", "zh")
+	if err != nil {
+		fmt.Println(err)
+		chinese = ""
+	}
 	return &response.EmployeeResponse{
 		ID:             employee.ID,
 		Name:           employee.Name,
@@ -30,7 +35,8 @@ func ConvertToSingleEmployeeResponse(employee *entity.Employee) *response.Employ
 			"name":                     employee.EmployeeJob.Name,
 			"emp_organization_id":      employee.EmployeeJob.EmpOrganizationID,
 			"job_id":                   employee.EmployeeJob.JobID,
-			"job_name":                 employee.EmployeeJob.Job.Name + " " + result,
+			"job_name":                 employee.EmployeeJob.Job.Name,
+			"job_name_chinese":         chinese,
 			"employee_id":              employee.EmployeeJob.EmployeeID,
 			"organization_location_id": employee.EmployeeJob.OrganizationLocationID,
 		},
