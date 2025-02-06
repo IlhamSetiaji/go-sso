e952485164de87f73087f98d92b8c6b264bffedf
thethreestrikes <ilham.ahmadz18@student.uns.ac.id>
2025-02-06 10:55:45 +0700

    feat: Implement login redirection for Applicant role with application token

--- internal/http/handler/web/auth_handler.go
+++ internal/http/handler/web/auth_handler.go
@@ -204,6 +204,29 @@ func (h *AuthHandler) ContinueLogin(ctx *gin.Context) {
 		return
 	}
 
+	if filteredRoles[0].Name == "Applicant" {
+		data, err := h.loginAsApplication(token, "recruitment", response.User)
+		if err != nil {
+			session.Set("error", err.Error())
+			session.Save()
+			h.Log.Printf(err.Error())
+			ctx.Redirect(302, ctx.Request.Referer())
+			return
+		}
+
+		application := data["application"].(*entity.Application)
+
+		redirectURL := fmt.Sprintf("%s?token=%s", application.RedirectURI, data["token"])
+		h.Log.Printf("Redirecting to URL: %s", redirectURL)
+
+		if !strings.HasPrefix(redirectURL, "http") {
+			redirectURL = "http://" + redirectURL
+		}
+
+		ctx.Redirect(302, redirectURL)
+		return
+	}
+
 	ctx.Redirect(302, "/portal")
 
 }
